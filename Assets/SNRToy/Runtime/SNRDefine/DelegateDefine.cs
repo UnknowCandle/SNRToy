48ddeaf542a2a9c48c933098997d4d2871666bc1
Mac_huang <huangmaotao@qq.com>
2024-04-30 14:25:03 +0800

    1

--- Assets/SNRToy/Runtime/SNRDefine/DelegateDefine.cs
+++ Assets/SNRToy/Runtime/SNRDefine/DelegateDefine.cs
@@ -1,10 +1,10 @@
 
 
 
-public delegate RT Delegate1P<RT, T>(T t);
-public delegate RT Delegate2P<RT, T1, T2>(T1 t1, T2 t2);
-public delegate RT Delegate3P<RT, T1, T2, T3>(T1 t1, T2 t2, T3 t3);
+public delegate void DelRtnVoid1P<T>(T t);
+public delegate void DelRtnVoid2P<T1, T2>(T1 t1, T2 t2);
+public delegate void DelRtnVoid3P<T1, T2, T3>(T1 t1, T2 t2, T3 t3);
 
 
-public delegate RT Delegate1More<RT, T>(params T[] ts);
-public delegate RT Delegate2More<RT, T1, T2>(T1 t1, params T2[] t2s);
+public delegate void DelRtnVoid1More<T>(params T[] ts);
+public delegate void DelRtnVoid2More<T1, T2>(T1 t1, params T2[] t2s);
