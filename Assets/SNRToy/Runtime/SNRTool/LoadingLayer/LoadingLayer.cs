987cbe0329356fc229ccc6879ed575c33ad87749
Mac_huang <huangmaotao@qq.com>
2024-04-30 17:26:39 +0800

    1

--- Assets/SNRToy/Runtime/SNRTool/LoadingLayer/LoadingLayer.cs
+++ Assets/SNRToy/Runtime/SNRTool/LoadingLayer/LoadingLayer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using SNRKWordDefine;
@@ -11,30 +12,46 @@ public class LoadingLayer : MonoBehaviour
 
     private int _fadeInId;
     private int _fadeOutId;
+
+    public Action BeforeFadeInAni { get; set; } = null;
+    public Action BeforeFadeOutAni { get; set; } = null;
+
+
     // Start is called before the first frame update
     void Start()
     {
         _fadeInId = Animator.StringToHash(KWord.LoadFadeIn);
         _fadeOutId = Animator.StringToHash(KWord.LoadFadeOut);
         _animator = GetComponent<Animator>();
-        
-        if(_fadeOutOnStrat){
+
+        if (_fadeOutOnStrat)
+        {
             FadeOut();
         }
-        
+
     }
 
-    public void FadeIn(){
+    public void FadeIn()
+    {
+        if (BeforeFadeInAni != null)
+        {
+            BeforeFadeInAni();
+        }
         _animator.Play(_fadeInId);
     }
 
-    public void FadeOut(){
+    public void FadeOut()
+    {
+        if (BeforeFadeOutAni != null)
+        {
+            BeforeFadeOutAni();
+        }
         _animator.Play(_fadeOutId);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
